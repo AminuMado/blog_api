978a53721e98d5b7041004ab1b7dba795bb2a950
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-18 13:42:50 +0100

    add connection to dababase

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const blogRoutes = require("./routes/blogs");
 
 // express app
 const app = express();
 
-// middleware
+/**
+ * -------------- DATABASE ----------------
+ */
+
+/**
+ * Connect to MongoDB Server using the connection string in the `.env` file.
+ * To implement this, place the following string into the `.env` file
+ * MONGODB_URI=mongodb://<user>:<password>@localhost:27017/database_name
+ */
+const dbString = process.env.MONGODB_URI;
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose
+  .connect(dbString, dbOptions)
+  .then(() => {
+    console.log("connected to database");
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    const db = mongoose.connection;
+    //Bind connection to error event (to get notification of connection errors)
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  });
+
+/**
+ * -------------- MIDDLEWARE ----------------
+ */
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -14,10 +44,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// routes
-app.use("/api/blogs", blogRoutes);
+/**
+ * -------------- ROUTES ----------------
+ */
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+app.use("/api/blogs", blogRoutes);
