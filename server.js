a0b57e2f9ddfe6df5b19edf2c65262916bf3920b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-18 13:10:36 +0100

    add blog routes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 
 const express = require("express");
+const blogRoutes = require("./routes/blogs");
 
+// express app
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello App");
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
+
+// routes
+app.use("/api/blogs", blogRoutes);
+
+// listen for requests
 app.listen(process.env.PORT, () => {
-  console.log("Listening on port: " + process.env.PORT);
+  console.log("listening on port", process.env.PORT);
 });
