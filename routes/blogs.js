3d8567065e58380ffc89068766f3252ee5fde6dc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-09-19 22:54:09 +0100

    fix code structure

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,27 +2,32 @@ const express = require("express");
 
 const router = express.Router();
 
-// GET all blogs
+/* --------- Get all Blogs --------- */
+
 router.get("/", (req, res) => {
   res.json({ message: "GET all blogs" });
 });
 
-// GET a single blog
+/* --------- Get a single Blog --------- */
+
 router.get("/:id", (req, res) => {
   res.json({ message: "GET a single blog" });
 });
 
-// POST/CREATE a new blog
+/* --------- Create a Blog --------- */
+
 router.post("/", (req, res) => {
   res.json({ message: "POST/CREATE a new blog" });
 });
 
-// UPDATE a blog
+/* --------- Update a Blog --------- */
+
 router.patch("/:id", (req, res) => {
   res.json({ message: "UPDATE a blog" });
 });
 
-// DELETE a blog
+/* --------- Delete a Blog --------- */
+
 router.delete("/:id", (req, res) => {
   res.json({ message: "DELETE a blog" });
 });
