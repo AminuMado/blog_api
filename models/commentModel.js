b72113e4218541fac5d517c8c8acf4ab8d84f01d
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-06 21:02:57 +0100

    change author property to username

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema(
   {
-    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    username: { type: String, required: true },
     content: { type: String, required: true },
     blog: { type: Schema.Types.ObjectId, ref: "Blog", required: true },
   },
