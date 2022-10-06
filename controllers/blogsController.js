ca6ea939e491182b5f03167c62ae26be02d25553
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-06 21:03:39 +0100

    comment out unused code

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -8,11 +8,8 @@ const Comment = require("../models/commentModel");
 /* --------- Get all Blogs --------- */
 
 const getBlogs = async (req, res) => {
-  const blogs = await Blog.find({})
-    .sort({ createdAt: -1 })
-    .populate("author")
-    .populate("comments")
-    .populate("likes");
+  const blogs = await Blog.find({}).sort({ createdAt: -1 }).populate("author");
+  // .populate("comments");
   res.status(200).json(blogs);
 };
 
@@ -27,12 +24,14 @@ const getBlog = async (req, res) => {
   }
 
   // Find the Blog
-  const blog = await Blog.findById(id).populate("comments").populate("likes");
+  const blog = await Blog.findById(id).populate("author").populate("comments");
+
   if (!blog) {
     return res.status(404).json({ error: "Blog not found" });
   }
 
   // Success... Blog has been found.
+
   res.status(200).json(blog);
 };
 
@@ -117,51 +116,49 @@ const deleteBlog = async (req, res) => {
   res.status(200).json(blog);
 };
 
-/* --------- Like a Blog --------- */
+// /* --------- Like a Blog --------- */
 
-const likeBlog = async (req, res) => {
-  const { id } = req.params;
-  // We need to find the currentuser from the jwt token but for now i am hardcoding the my user Id
-  const userId = "63298c7b5ae2a003d32fd904";
-  // Check if ID is valid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Blog not Found" });
-  }
-  const blog = await Blog.findOneAndUpdate(
-    { _id: id },
-    { $addToSet: { likes: userId } }
-  );
-  if (!blog) {
-    return res.status(400).json({ error: "Blog not found" });
-  }
-  return res.status(200).json(blog);
-};
+// const likeBlog = async (req, res) => {
+//   const { id } = req.params;
+//   // We need to find the currentuser from the jwt token but for now i am hardcoding the my user Id
+//   const userId = "63298c7b5ae2a003d32fd904";
+//   // Check if ID is valid
+//   if (!mongoose.Types.ObjectId.isValid(id)) {
+//     return res.status(400).json({ error: "Blog not Found" });
+//   }
+//   const blog = await Blog.findOneAndUpdate(
+//     { _id: id },
+//     { $addToSet: { likes: userId } }
+//   );
+//   if (!blog) {
+//     return res.status(400).json({ error: "Blog not found" });
+//   }
+//   return res.status(200).json(blog);
+// };
 
-/* --------- Unlike a Blog --------- */
+// /* --------- Unlike a Blog --------- */
 
-const unLikeBlog = async (req, res) => {
-  const { id } = req.params;
-  // We need to find the currentuser from the jwt token but for now i am hardcoding the my user Id
-  const userId = "63298c7b5ae2a003d32fd904";
-  // Check if ID is valid
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Blog not Found" });
-  }
-  const blog = await Blog.findOneAndUpdate(
-    { _id: id },
-    { $pull: { likes: userId } }
-  );
-  if (!blog) {
-    return res.status(400).json({ error: "Blog not found" });
-  }
-  return res.status(200).json(blog);
-};
+// const unLikeBlog = async (req, res) => {
+//   const { id } = req.params;
+//   // We need to find the currentuser from the jwt token but for now i am hardcoding the my user Id
+//   const userId = "63298c7b5ae2a003d32fd904";
+//   // Check if ID is valid
+//   if (!mongoose.Types.ObjectId.isValid(id)) {
+//     return res.status(400).json({ error: "Blog not Found" });
+//   }
+//   const blog = await Blog.findOneAndUpdate(
+//     { _id: id },
+//     { $pull: { likes: userId } }
+//   );
+//   if (!blog) {
+//     return res.status(400).json({ error: "Blog not found" });
+//   }
+//   return res.status(200).json(blog);
+// };
 module.exports = {
   getBlogs,
   getBlog,
   createBlog,
   deleteBlog,
   updateBlog,
-  likeBlog,
-  unLikeBlog,
 };
