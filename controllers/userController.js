b4042838b71840621a5cf136c5fc17c90ed53bb9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-10-16 19:53:41 +0100

    add login method

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,20 @@ const createToken = (_id) => {
 /* --------- Log In --------- */
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  // Get the username email and password from the request
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const username = user.username;
+
+    // create a token
+    const token = createToken(user._id);
+
+    // respond with the token and username
+    res.status(200).json({ username, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 /* --------- Sign Up --------- */
@@ -17,9 +30,11 @@ const signupUser = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const user = await User.signup(username, email, password);
+
     // Create a Token
     const token = createToken(user._id);
 
+    // Respond with the username and token
     res.status(200).json({ username, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
